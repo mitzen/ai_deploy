a361f87575eb7d69474ad4278f0b47ae9b61ef03
jeremy woo <jeremy>
2022-11-01 22:38:02 +1300

    gunicorn setup

--- dockerfile
+++ dockerfile
@@ -1,5 +1,6 @@
 FROM python:3.9.0-buster
 
+EXPOSE 8000
 COPY requirements.txt /
 
 RUN pip install -r /requirements.txt
--- main.py
+++ main.py
@@ -72,4 +72,6 @@ if __name__ == "__main__":
 	print(("* Loading Keras model and Flask starting server..."
 		"please wait until server has fully started"))
 	load_model()
-	app.run()
+	app.run()	
+
+load_model()
